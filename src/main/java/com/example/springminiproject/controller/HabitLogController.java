ba60021d6366065ed33a3cef8c526cb40403ae76
Dane Lim <144203132+danelim1203@users.noreply.github.com>
2026-04-03 16:52:54 +0700

    get habit by id

---
+++ src/main/java/com/example/springminiproject/controller/HabitLogController.java
@@ -0,0 +1,49 @@
+package com.example.springminiproject.controller;
+
+import com.example.springminiproject.exception.NotFoundException;
+import com.example.springminiproject.model.entity.HabitLog;
+import com.example.springminiproject.model.response.ApiResponse;
+import com.example.springminiproject.service.HabitLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/hobit-logs")
+public class HabitLogController {
+
+    private final HabitLogService habitLogService;
+
+    @GetMapping("{habit-id}")
+    public ResponseEntity<ApiResponse<HabitLog>> getHobitLogById(
+            @PathVariable("habit-id") UUID habitLogId) {
+
+        try {
+            HabitLog habitLog = habitLogService.getHabitLogById(habitLogId);
+
+            ApiResponse<HabitLog> response = ApiResponse.<HabitLog>builder()
+                    .isSuccess(true)
+                    .status(HttpStatus.OK)
+                    .message("Get HabitLog By ID Successfully")
+                    .payload(habitLog)
+                    .timestamp(Instant.now()).build();
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (NotFoundException e){
+            ApiResponse<HabitLog> response = ApiResponse.<HabitLog>builder()
+                    .isSuccess(false)
+                    .status(HttpStatus.NOT_FOUND)
+                    .message(e.getMessage())
+                    .payload(null)
+                    .timestamp(Instant.now())
+                    .build();
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+    }
+}
