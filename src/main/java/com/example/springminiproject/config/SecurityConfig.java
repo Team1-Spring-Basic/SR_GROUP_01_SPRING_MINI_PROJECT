e0a2f91bb13af28c0a86b2b5789d6d20deeda9d9
MAOMEANMEAN <mao.meanmean.1222@rupp.edu.kh>
2026-04-04 12:36:11 +0700

    my local change

--- src/main/java/com/example/springminiproject/config/SecurityConfig.java
+++ src/main/java/com/example/springminiproject/config/SecurityConfig.java
@@ -1,4 +1,17 @@
 package com.example.springminiproject.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityconfig(HttpSecurity http){
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .build();
+    }
 }
