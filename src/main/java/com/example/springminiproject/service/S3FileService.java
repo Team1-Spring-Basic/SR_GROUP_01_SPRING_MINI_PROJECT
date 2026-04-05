d9e794762dcdf70d757fa6ed2c76f2a3716d3304
Nut Youlong <Nutyoulong@gmail.com>
2026-04-05 18:31:10 +0700

    add handle exception for file not found in s3

--- src/main/java/com/example/springminiproject/service/S3FileService.java
+++ src/main/java/com/example/springminiproject/service/S3FileService.java
@@ -1,10 +1,11 @@
 package com.example.springminiproject.service;
 
 import com.example.springminiproject.model.entity.FileMetaData;
+import org.apache.coyote.BadRequestException;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3FileService {
     FileMetaData uploadFile(MultipartFile file);
-    Resource getFileByFileName(String fileName);
+    Resource getFileByFileName(String fileName) throws BadRequestException;
 }
